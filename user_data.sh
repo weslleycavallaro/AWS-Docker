cd08d761f1553c0e18cd2f47ad159855ec053416
weslleycavallaro <weslley2013cavallaro@gmail.com>
2025-05-07 09:52:45 -0300

    Atualizando o readme

--- user_data.sh
+++ user_data.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 set -e
 
-sudo apt update -y && sudo apt install -y \
+sudo mkdir -p /home/aws
+sudo chown ubuntu:ubuntu /home/aws
+
+
+sudo apt update -y
+sudo apt install -y ca-certificates curl gnupg lsb-release
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+  sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update -y
+sudo apt install -y \
   awscli \
-  ca-certificates \
-  curl \
-  gnupg \
   nfs-common \
   docker-ce \
   docker-ce-cli \
@@ -13,35 +28,28 @@ sudo apt update -y && sudo apt install -y \
   docker-buildx-plugin \
   docker-compose-plugin
 
-cat <<EOF > /home/aws/var.env
-WORDPRESS_DB_HOST=db-projeto-docker.cpim24s04axt.us-east-1.rds.amazonaws.com:3306
-WORDPRESS_DB_USER=weslley
-WORDPRESS_DB_PASSWORD=teste#teste
+
+# Variáveis
+cat <<EOF > $USER_HOME/var.env
+WORDPRESS_DB_HOST=db-projeto-docker.12345.us-east-1.rds.amazonaws.com:3306
+WORDPRESS_DB_USER=admin
+WORDPRESS_DB_PASSWORD=senha#senha
 WORDPRESS_DB_NAME=DBPROJETODOCKER
-WORDPRESS_SITEURL=http://projetodocker-1253802345.us-east-1.elb.amazonaws.com
-WORDPRESS_HOME=http://projetodocker-1253802345.us-east-1.elb.amazonaws.com
-EFS_DNS=fs-0644d31a54d3cd896.efs.us-east-1.amazonaws.com
+WORDPRESS_SITEURL=http://projetodocker-12345.us-east-1.elb.amazonaws.com
+WORDPRESS_HOME=http://projetodocker-12345.us-east-1.elb.amazonaws.com
+EFS_DNS=fs-12345.efs.us-east-1.amazonaws.com
 EOF
 
-set -a
 source /home/aws/var.env
-set +a
 
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-  sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) \
-  signed-by=/etc/apt/keyrings/docker.gpg] \
-  https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# EFS
 sudo mkdir -p /mnt/efs/wordpress
 sudo mount -t nfs4 -o nfsvers=4.1,tcp,hard,timeo=600,retrans=2 "$EFS_DNS":/ /mnt/efs
-
 sudo chown -R 33:33 /mnt/efs/wordpress
 
+
+# Compose
 cat <<EOF > /home/aws/compose.yml
 services:
   wordpress:
@@ -58,9 +66,8 @@ services:
       WORDPRESS_HOME: ${WORDPRESS_HOME}
     volumes:
       - /mnt/efs/wordpress:/var/www/html
-
 EOF
 
-cd /home/aws
 
+cd /home/aws
 sudo docker compose up -d
