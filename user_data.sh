cb8cda9d6e31eba65488a6f6643a20dabab2df84
weslleycavallaro <weslley2013cavallaro@gmail.com>
2025-05-07 23:31:39 -0300

    Atualizando userdata

--- user_data.sh
+++ user_data.sh
@@ -1,56 +1,62 @@
 #!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
 set -e
+
+export EFS_DNS=fs-12345.efs.us-east-1.amazonaws.com
+
 
 sudo mkdir -p /home/aws
 sudo chown ubuntu:ubuntu /home/aws
 
 
-sudo apt update -y
-sudo apt install -y ca-certificates curl gnupg lsb-release
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+
 
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-  sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
   https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update -y
-sudo apt install -y \
-  awscli \
-  nfs-common \
-  docker-ce \
-  docker-ce-cli \
-  containerd.io \
-  docker-buildx-plugin \
-  docker-compose-plugin
 
 
-# Variáveis
-cat <<EOF > $USER_HOME/var.env
-WORDPRESS_DB_HOST=db-projeto-docker.12345.us-east-1.rds.amazonaws.com:3306
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+
+sudo tee /home/aws/.env > /dev/null <<EOF
+WORDPRESS_DB_HOST=rdsprojetodocker.12345.us-east-1.rds.amazonaws.com:3306
 WORDPRESS_DB_USER=admin
-WORDPRESS_DB_PASSWORD=senha#senha
-WORDPRESS_DB_NAME=DBPROJETODOCKER
-WORDPRESS_SITEURL=http://projetodocker-12345.us-east-1.elb.amazonaws.com
-WORDPRESS_HOME=http://projetodocker-12345.us-east-1.elb.amazonaws.com
-EFS_DNS=fs-12345.efs.us-east-1.amazonaws.com
+WORDPRESS_DB_PASSWORD=senha
+WORDPRESS_DB_NAME=rdsprojetodocker
+WORDPRESS_SITEURL=http://loadbalancer
+WORDPRESS_HOME=http://loadbalancer
 EOF
 
-source /home/aws/var.env
+source /home/aws/.env
 
 
-# EFS
+
 sudo mkdir -p /mnt/efs/wordpress
+sudo apt-get install -y nfs-common
+
 sudo mount -t nfs4 -o nfsvers=4.1,tcp,hard,timeo=600,retrans=2 "$EFS_DNS":/ /mnt/efs
 sudo chown -R 33:33 /mnt/efs/wordpress
 
 
-# Compose
-cat <<EOF > /home/aws/compose.yml
+
+sudo tee /home/aws/compose.yml > /dev/null <<EOF
 services:
   wordpress:
     image: wordpress
@@ -58,16 +64,18 @@ services:
     ports:
       - 80:80
     environment:
-      WORDPRESS_DB_HOST: ${WORDPRESS_DB_HOST}
-      WORDPRESS_DB_USER: ${WORDPRESS_DB_USER}
-      WORDPRESS_DB_PASSWORD: ${WORDPRESS_DB_PASSWORD}
-      WORDPRESS_DB_NAME: ${WORDPRESS_DB_NAME}
-      WORDPRESS_SITEURL: ${WORDPRESS_SITEURL}
-      WORDPRESS_HOME: ${WORDPRESS_HOME}
+      WORDPRESS_DB_HOST: \${WORDPRESS_DB_HOST}
+      WORDPRESS_DB_USER: \${WORDPRESS_DB_USER}
+      WORDPRESS_DB_PASSWORD: \${WORDPRESS_DB_PASSWORD}
+      WORDPRESS_DB_NAME: \${WORDPRESS_DB_NAME}
+      WORDPRESS_SITEURL: \${WORDPRESS_SITEURL}
+      WORDPRESS_HOME: \${WORDPRESS_HOME}
     volumes:
       - /mnt/efs/wordpress:/var/www/html
 EOF
 
-
 cd /home/aws
+
+sudo systemctl enable docker
+sudo systemctl start docker
 sudo docker compose up -d
